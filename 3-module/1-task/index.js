fab9e363ea2db3c81174c851dff55364910f3ed0
Roman Misyurin <roman.misyurin@teligent.ru>
2019-06-12 22:15:26 +0300

    created showSalary function

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -4,7 +4,15 @@
  * @param {number} age - максимальный возраст
  * @returns {string}
  */
-function showSalary(data, age) {
+
+let showSalary = function(data, age) {
+    var newArray = data.filter(function(currentEmployee){
+        return currentEmployee.age <= age;
+    });
+
+    var finalArray = newArray.map(function(currentEmployee){
+        return currentEmployee.name + ', ' + currentEmployee.balance;
+    })
+
+    return finalArray.join('\n');
 }
-
-
